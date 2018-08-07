0ab85b032637501b414c3c2302b537974e3d023f
Lewis Cowper <lewis.cowper@googlemail.com>
2018-08-07 16:22:19 +0200

    updates for new MBP

--- install.sh
+++ install.sh
@@ -84,6 +84,29 @@ then
   stow zsh
 fi
 
+# create vim symlinks
+# As I have moved off of vim as my full time editor in favor of neovim,
+# I feel it doesn't make sense to leave my vimrc intact in the dotfiles repo
+# as it is not really being actively maintained. However, I would still
+# like to configure vim, so lets symlink ~/.vimrc and ~/.vim over to their
+# neovim equivalent.
+
+fancy_echo "Creating vim symlinks"
+fancy_echo "=============================="
+VIMFILES=( "$HOME/.vim:$HOME/.config/nvim"
+        "$HOME/.vimrc:$HOME/.config/nvim/init.vim" )
+
+for file in "${VIMFILES[@]}"; do
+    KEY=${file%%:*}
+    VALUE=${file#*:}
+    if [ -e "${KEY}" ]; then
+        fancy_echo "${KEY} already exists... skipping."
+    else
+        fancy_echo "Creating symlink for $KEY"
+        ln -s "${VALUE}" "${KEY}"
+    fi
+done
+
 # vim-plug
 if [ ! -e ~/.config/nvim/autoload/plug.vim ]; then
   fancy_echo "Installing vim-plug"
--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,17 +1,20 @@
-alias ls='ls -G'
+alias ls='colorls -lA --sd'
 alias cp='cp -i'
 alias mv='mv -i'
 alias grep='grep --colour=auto'
-alias rm=trash
 alias vim=nvim
+alias e="$EDITOR"
+alias ta="tmux attach -t"
+alias tn="tmux new-session -t"
+alias tls="tmux list-sessions"
 alias untar='tar xvf'
 alias tmux='TERM=xterm-256color tmux -2 -f $XDG_CONFIG_HOME/tmux/conf'
 alias weechat='weechat -d $XDG_CONFIG_HOME/weechat'
 alias gpg='gpg --homedir $XDG_CONFIG_HOME/gnupg'
 alias sz='source ~/.zshrc'
 alias release='echo "release-$(date +%y%V).0"'
-alias week='date +%V'
 alias sudo='sudo '
 alias clean-docker-containers='docker rm $(docker ps -aq)'
 alias clean-docker-images='docker rmi $(docker images -q)'
 alias clean-selenium="kill $(ps aux | grep 'selenium-standalon[e]' | awk '{print $2}')"
+alias did="vim +'normal Go' +'r!date' ~/did.txt"
